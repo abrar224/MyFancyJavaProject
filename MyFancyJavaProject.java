892ce8342758be7da3a7439d392e6cbd3dd0dae5
Abrar Sadiq <airshad181224@bscse.uiu.ac.bd>
2021-09-05 13:45:30 +0600

    MyFancyJavaProject.java file is added without using Singleton pattern

---
+++ MyFancyJavaProject.java
@@ -0,0 +1,27 @@
+package SoftwareAssignment;
+
+class DbConnection{
+    String dbUser;
+    String dbPassword;
+    String dbName;
+    DbConnection(String dbUser, String dbPassword, String dbName) {
+        this.dbUser = dbUser;
+        this.dbPassword = dbPassword;
+        this.dbName = dbName;
+    }
+
+    void printAll(){
+        System.out.println("User: " + dbUser + "\nPassword: " + dbPassword + "\nDBName: " + dbName);
+    }
+}
+
+public class MyFancyJavaProject {
+
+    public static void main(String[] args){
+        String dbUser="admin";
+        String dbPassword="admin";
+        String dbName="testDb";
+        DbConnection connection = new DbConnection(dbUser, dbPassword, dbName);
+        connection.printAll();
+    }
+}

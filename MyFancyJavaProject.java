0ccb02a051c5b551be2bb48d3d11b986293c6783
Emran144 <mhossain181144@bscse.uiu.ac.bd>
2021-09-07 21:23:55 +0600

    modify in singleton pattern

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -1,27 +1,41 @@
-package SoftwareAssignment;
-
 class DbConnection{
+
+    private static DbConnection instance=null;
+
+    private DbConnection(String dbUser,String dbPassword,String dbName ) {
+        SQLConnection connection = new SQLConnection(dbUser, dbPassword, dbName);
+    }
+
+
+    public static DbConnection getDbConnection(String dbUser,String dbPassword,String dbName) {
+        if (instance== null )
+            instance = new DbConnection(dbUser,dbPassword,dbName) ;
+
+        return instance;
+    }
+}
+
+class SQLConnection {
     String dbUser;
     String dbPassword;
     String dbName;
-    DbConnection(String dbUser, String dbPassword, String dbName) {
+
+    public SQLConnection(String dbUser, String dbPassword, String dbName) {
+        super();
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
         this.dbName = dbName;
     }
-
-    void printAll(){
-        System.out.println("User: " + dbUser + "\nPassword: " + dbPassword + "\nDBName: " + dbName);
-    }
 }
 
-public class MyFancyJavaProject {
-
-    public static void main(String[] args){
+class Test
+{
+    public static void main(String[] args) throws Exception
+    {
         String dbUser="admin";
         String dbPassword="admin";
-        String dbName="testDb";
-        DbConnection connection = new DbConnection(dbUser, dbPassword, dbName);
-        connection.printAll();
+        String dbName="tesDb";
+        DbConnection connection = DbConnection.getDbConnection(dbUser, dbPassword, dbName);
     }
 }
+
